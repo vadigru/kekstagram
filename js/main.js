a91609a817cda1a48f07d2f8ddf4b22f8bbc90e2
Vadim Grunenko <vg@outlook.ie>
2020-01-26 23:28:22 +0200

    Generates random data and renders pictures

---
+++ js/main.js
@@ -0,0 +1,91 @@
+'use strict';
+var COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+var COMMENTATORS = [
+  'Люсинда Гомес',
+  'Чау Расма',
+  'Нил Армстронг',
+  'Ахмед Аль Ахмед',
+  'Петр Первый',
+  'Вам Лучше Не Знать'
+];
+var DESCRIPTION = [
+  'Прекрасный вид прекрасного вида',
+  'Зачётно провели время',
+  'Расслабляемся по-полной',
+  'Лучше просто не бывает',
+  'Эти воспоминания ничем не стереть',
+  'Так просто потому что можем'
+];
+var POSTS_COUNT = 25;
+var MIN = 0;
+var MIN_LIKES = 15;
+var MAX_LIKES = 200;
+var MIN_AVATAR = 1;
+var MAX_AVATAR = 6;
+var MIN_COMMENTS = 1;
+var userPosts = [];
+var otherPics = document.querySelector('.pictures');
+
+// gets random element from array. gets random number in range. ---------------
+var getRandomValue = function (min, array) {
+  var max = array;
+  return Array.isArray(array) === true ?
+    array[Math.floor(Math.random() * max.length)] :
+    Math.floor(min + Math.random() * (max + 1 - min));
+};
+
+// generates random data for userPosts ----------------------------------------
+var generateData = function (count) {
+  var photoInfo = {};
+  var generateComments = function (array) {
+    var userComments = [];
+    for (var j = 0; j < getRandomValue(MIN_COMMENTS, array.length); j++) {
+      var comment = {
+        'avatar': 'img/avatar-' + getRandomValue(MIN_AVATAR, MAX_AVATAR) + '.svg',
+        'message': getRandomValue(MIN, COMMENTS),
+        'name': getRandomValue(MIN, COMMENTATORS)
+      };
+      userComments.push(comment);
+    }
+    return userComments;
+  };
+  for (var i = 1; i <= count; i++) {
+    photoInfo = {
+      'url': 'photos/' + i + '.jpg',
+      'description': getRandomValue(MIN, DESCRIPTION),
+      'likes': getRandomValue(MIN_LIKES, MAX_LIKES),
+      'comments': generateComments(COMMENTS)
+    };
+    userPosts.push(photoInfo);
+  }
+};
+
+generateData(POSTS_COUNT);
+
+// renders posts using generated data from userPosts --------------------------
+var renderPost = function (data) {
+  var template = document.querySelector('#picture').content.querySelector('.picture');
+  var templateElement = template.cloneNode(true);
+  templateElement.querySelector('.picture__img').src = data.url;
+  templateElement.querySelector('.picture__likes').textContent = data.likes;
+  templateElement.querySelector('.picture__comments').textContent = data.comments.length;
+  return templateElement;
+};
+
+// creates fragment using generated data from userPosts -----------------------
+var buildFragment = function (array) {
+  var fragment = document.createDocumentFragment();
+  array.forEach(function (item) {
+    fragment.appendChild(renderPost(item));
+  });
+  return fragment;
+};
+
+otherPics.appendChild(buildFragment(userPosts));
